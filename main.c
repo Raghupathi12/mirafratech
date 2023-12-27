c6b640e4eb08a4b6094a26fa3ca34efbb333ac96
raghu <raghupathisudula456@gmail.com>
2023-12-27 12:50:48 +0530

    missed ones which add and not commited

--- main.c
+++ main.c
@@ -4,8 +4,11 @@ int main()
 int a=10,b=20;
 int c;
 printf("welcome\n");
+<<<<<<< HEAD
 printf("dont take anything pesonally\n");
 printf("%d\n %d\n",c=a+b);
 
+=======
+>>>>>>> parent of bb96674... second_commit
 }
 
